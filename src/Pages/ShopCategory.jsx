b2eacd74fe47d91983bff84dd280315283b91d08
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-08-12 00:07:29 +0530

    display products

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
 
-const ShopCategory = () => {
+import React, { useContext } from 'react'
+import './Css/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/assets/dropdown_icon.png'
+import Item from '../Components/Items/Item'
+
+const ShopCategory = (props) => {
+  const {all_product} =useContext(ShopContext)
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img className='shop-category-banner' src={props.banner} alt="" />
+      <div className="shop-category-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className='shop-category-sort'>
+          Sort by <img src={dropdown_icon} alt=''/>
+        </div>
+      </div>
+      <div className="shop-category-product">
+        {all_product.map((item,i)=>{
+          if (props.category===item.category) {
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          }
+          else{
+            return null;
+          }
+        })}
+        </div>
     </div>
   )
 }
